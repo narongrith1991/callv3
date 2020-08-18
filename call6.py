9ac4b07081a288f3c30711e59325a48a376e7ba1
narongrith1991 <61232522+narongrith1991@users.noreply.github.com>
2020-08-18 22:37:30 +0700

    Update call6.py

--- call6.py
+++ call6.py
@@ -33,6 +33,6 @@ for i in range(1,6):
         print ("\033[90m> \]33[1;97mSPAM \033[1;94m=> \033[1;91mFAILED")
     else:
         print ("\033[90m> \033[1;97mSPAM \033[1;94m=> \033[1;92mSUCCESS")
-    time.sleep(1000)
+    time.sleep(10)
 
 balik()
